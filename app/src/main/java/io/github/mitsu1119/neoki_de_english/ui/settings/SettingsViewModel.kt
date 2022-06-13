121e1c49ab787e65fa489c3107c9c92565b682c1
mitsu <hachimitsu1119@gmail.com>
2022-06-13 19:14:12 +0900

    Initial commit

---
+++ app/src/main/java/io/github/mitsu1119/neoki_de_english/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,13 @@
+package io.github.mitsu1119.neoki_de_english.ui.settings
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is settings Fragment"
+    }
+    val text: LiveData<String> = _text
+}
