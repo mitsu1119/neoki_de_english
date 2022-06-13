20515036232490d743e59235c76161c113d6b9bb
mitsu <hachimitsu1119@gmail.com>
2022-06-13 20:14:25 +0900

    勝ち勝ち山と申します

--- app/src/main/java/io/github/mitsu1119/neoki_de_english/ui/transform/TransformFragment.kt
+++ app/src/main/java/io/github/mitsu1119/neoki_de_english/ui/transform/TransformFragment.kt
@@ -63,25 +63,6 @@ class TransformFragment : Fragment() {
                 oldItem == newItem
         }) {
 
-        private val drawables = listOf(
-            R.drawable.avatar_1,
-            R.drawable.avatar_2,
-            R.drawable.avatar_3,
-            R.drawable.avatar_4,
-            R.drawable.avatar_5,
-            R.drawable.avatar_6,
-            R.drawable.avatar_7,
-            R.drawable.avatar_8,
-            R.drawable.avatar_9,
-            R.drawable.avatar_10,
-            R.drawable.avatar_11,
-            R.drawable.avatar_12,
-            R.drawable.avatar_13,
-            R.drawable.avatar_14,
-            R.drawable.avatar_15,
-            R.drawable.avatar_16,
-        )
-
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransformViewHolder {
             val binding = ItemTransformBinding.inflate(LayoutInflater.from(parent.context))
             return TransformViewHolder(binding)
@@ -89,16 +70,12 @@ class TransformFragment : Fragment() {
 
         override fun onBindViewHolder(holder: TransformViewHolder, position: Int) {
             holder.textView.text = getItem(position)
-            holder.imageView.setImageDrawable(
-                ResourcesCompat.getDrawable(holder.imageView.resources, drawables[position], null)
-            )
         }
     }
 
     class TransformViewHolder(binding: ItemTransformBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        val imageView: ImageView = binding.imageViewItemTransform
         val textView: TextView = binding.textViewItemTransform
     }
 }
