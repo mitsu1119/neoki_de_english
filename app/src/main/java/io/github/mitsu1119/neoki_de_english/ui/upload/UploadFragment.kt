8992e76ce0500c2018d616184a65d5fbd3980724
mitsu <hachimitsu1119@gmail.com>
2022-07-11 21:03:39 +0900

    [add] Upload画面のSpinner

--- app/src/main/java/io/github/mitsu1119/neoki_de_english/ui/upload/UploadFragment.kt
+++ app/src/main/java/io/github/mitsu1119/neoki_de_english/ui/upload/UploadFragment.kt
@@ -2,17 +2,23 @@ package io.github.mitsu1119.neoki_de_english.ui.upload
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import io.github.mitsu1119.neoki_de_english.databinding.FragmentUploadBinding
+import io.github.mitsu1119.neoki_de_english.ui.dictionary.DictionaryViewModel
+import io.github.mitsu1119.neoki_de_english.ui.local_dictionary.LocalDictionaryViewModel
+import java.io.File
 
 class UploadFragment: Fragment() {
     private var _binding: FragmentUploadBinding? = null
-
     private val binding get() = _binding!!
+
+    private lateinit var internalDir: File
 
     @SuppressLint("UnspecifiedImmutableFlag")
     override fun onCreateView(
@@ -23,6 +29,16 @@ class UploadFragment: Fragment() {
         val transformViewModel = ViewModelProvider(this).get(UploadViewModel::class.java)
         _binding = FragmentUploadBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        internalDir = requireContext().filesDir
+
+        transformViewModel.loadDicNames(internalDir)
+
+        // 辞書選択
+        val spinnerItems = transformViewModel.dicNames.value!!.stream().toArray { arrayOfNulls<String>(it) }
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinnerItems)
+        val spnDic = binding.spnDic
+        spnDic.adapter = adapter
 
         val btnAlarm = binding.btnUpload
         btnAlarm.setOnClickListener { view ->
--- app/src/main/java/io/github/mitsu1119/neoki_de_english/ui/upload/UploadViewModel.kt
+++ app/src/main/java/io/github/mitsu1119/neoki_de_english/ui/upload/UploadViewModel.kt
@@ -1,6 +1,22 @@
 package io.github.mitsu1119.neoki_de_english.ui.upload
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.github.mitsu1119.neoki_de_english.dictionary.DicSet
+import java.io.File
 
 class UploadViewModel: ViewModel() {
+    private val _dicNames = MutableLiveData<MutableList<String>>().apply {
+        value = mutableListOf()
+    }
+    fun loadDicNames(internalDir: File) {
+        for(name in DicSet.getDicNames(internalDir)) {
+            if(!dicNames.value!!.contains(name)) addDicName(name)
+        }
+    }
+    fun addDicName(name: String) {
+        _dicNames.value?.add(name)
+    }
+    val dicNames: LiveData<MutableList<String>> = _dicNames
 }
