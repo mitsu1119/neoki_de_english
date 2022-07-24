770baed468c46ff2066145a4723eb5f1d7d0183d
mitsu <37875610+mitsu1119@users.noreply.github.com>
2022-07-25 04:13:59 +0900

    [add] ダウンロード画面にサーバの辞書一覧を表示

--- app/src/main/java/io/github/mitsu1119/neoki_de_english/ui/download/DownloadFragment.kt
+++ app/src/main/java/io/github/mitsu1119/neoki_de_english/ui/download/DownloadFragment.kt
@@ -7,12 +7,20 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import io.github.mitsu1119.neoki_de_english.MainActivity
 import io.github.mitsu1119.neoki_de_english.conn.Server
 import io.github.mitsu1119.neoki_de_english.databinding.FragmentDownloadBinding
+import io.github.mitsu1119.neoki_de_english.databinding.ItemDicDownloadBinding
 import io.github.mitsu1119.neoki_de_english.dictionary.DicSet
+import io.github.mitsu1119.neoki_de_english.ui.local_dictionary.LocalDictionaryFragment
+import io.github.mitsu1119.neoki_de_english.ui.local_dictionary.LocalDictionaryFragmentDirections
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
@@ -45,10 +53,26 @@ class DownloadFragment: Fragment(), TextToSpeech.OnInitListener {
 
         tts = TextToSpeech(requireContext(), this)
 
+        val recyclerView = binding.recyclerView
+        val adapter = DownloadAdapter()
+        recyclerView.adapter = adapter
+        transformViewModel.dicNames.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
+
         val server = Server(MainActivity().coroutineContext)
         val remoteDics = server.getWBlist()
+        val remoteDicList = remoteDics.split(",")
+        remoteDicList.forEach { Log.v("m2_download", it); transformViewModel.addDicName(it) }
 
-        server.downloadWB("dic1", internalDir)
+        adapter.itemClickListener = object: DownloadAdapter.OnItemClickListener {
+            override fun onItemClick(holder: DownloadViewHolder): Boolean {
+                Log.v("m2_download", "download... ${holder.textView.text}")
+                server.downloadWB(holder.textView.text.toString(), internalDir)
+                return true
+            }
+        }
+
 
         binding.button.setOnClickListener {
             createDicAudio("dic1")
@@ -89,5 +113,40 @@ class DownloadFragment: Fragment(), TextToSpeech.OnInitListener {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    class DownloadAdapter :
+        ListAdapter<String, DownloadViewHolder>(object : DiffUtil.ItemCallback<String>() {
+
+            override fun areItemsTheSame(oldItem: String, newItem: String): Boolean =
+                oldItem == newItem
+
+            override fun areContentsTheSame(oldItem: String, newItem: String): Boolean =
+                oldItem == newItem
+        }) {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DownloadViewHolder {
+            val binding = ItemDicDownloadBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return DownloadViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: DownloadViewHolder, position: Int) {
+            val name = getItem(position)
+            holder.textView.text = name
+            holder.itemView.setOnClickListener {
+                itemClickListener?.onItemClick(holder)
+            }
+        }
+
+        var itemClickListener: OnItemClickListener? = null
+        interface OnItemClickListener {
+            fun onItemClick(holder: DownloadViewHolder): Boolean
+        }
+    }
+
+    class DownloadViewHolder(binding: ItemDicDownloadBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        val textView: TextView = binding.textViewItemTransform
     }
 }
--- app/src/main/java/io/github/mitsu1119/neoki_de_english/ui/download/DownloadViewModel.kt
+++ app/src/main/java/io/github/mitsu1119/neoki_de_english/ui/download/DownloadViewModel.kt
@@ -1,6 +1,22 @@
 package io.github.mitsu1119.neoki_de_english.ui.download
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.github.mitsu1119.neoki_de_english.dictionary.DicSet
+import java.io.File
 
 class DownloadViewModel: ViewModel() {
+    private val _dicNames = MutableLiveData<MutableList<String>>().apply {
+        value = mutableListOf()
+    }
+    fun loadDicNames(internalDir: File) {
+        for(name in DicSet.getDicNames(internalDir)) {
+            if(!dicNames.value!!.contains(name)) addDicName(name)
+        }
+    }
+    fun addDicName(name: String) {
+        _dicNames.value?.add(name)
+    }
+    val dicNames: LiveData<MutableList<String>> = _dicNames
 }
